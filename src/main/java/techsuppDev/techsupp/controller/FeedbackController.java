19becba61146253162de40bbd88d897d9b8db6c7
thumbthing <ilscc0902@gmail.com>
2023-02-27 16:09:48 +0900

    feat: createFeedbackList.js (api 호출로 리스트 생성)

--- src/main/java/techsuppDev/techsupp/controller/FeedbackController.java
+++ src/main/java/techsuppDev/techsupp/controller/FeedbackController.java
@@ -3,16 +3,21 @@ package techsuppDev.techsupp.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-@Controller
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/feedbackMain")
 public class FeedbackController {
 
-// feedback service 만들어야함
 
 
-    @GetMapping("/feedbackMain")
-    public String LinkToFeedbackMain() {
-        return "/feedback/feedbackMain";
+    @GetMapping("/*")
+    public ModelAndView LinkToFeedbackMain() {
+        ModelAndView feedbackMain = new ModelAndView("/feedback/feedbackMain");
+        return feedbackMain;
     }
 
 

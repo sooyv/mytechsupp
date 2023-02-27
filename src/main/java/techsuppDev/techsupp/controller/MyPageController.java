db30146369be2dcdcfd6c0467a63a765ea765f33
moonbeom <tjansqja789@naver.com>
2023-02-27 17:25:14 +0900

    서문범 commit 2/27

--- src/main/java/techsuppDev/techsupp/controller/MyPageController.java
+++ src/main/java/techsuppDev/techsupp/controller/MyPageController.java
@@ -1,4 +1,61 @@
 package techsuppDev.techsupp.controller;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import techsuppDev.techsupp.domain.User;
+import techsuppDev.techsupp.service.MyPageService;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@AllArgsConstructor
 public class MyPageController {
+
+    private final MyPageService myPageService;
+
+//  회원수정하기 전 비밀번호 확인
+    @GetMapping("/checkPassword")
+    public String checkPwdView(){
+        return "mypage/checkPassword";
+    }
+//
+//    @PostMapping("/checkPassword")
+//    public boolean checkPassword(@RequestParam String checkPassword, Model model){
+//
+//
+//        return "mypage/checkPassword";
+//    }
+
+
+
+//    회원정보수정페이지
+    @GetMapping("/edituser") // modelandview 모델을 뷰에 던져준다는 개념임.
+    public String editUser(HttpSession session, Model model) {
+
+//        String myEmail = (String) session.getAttribute("userEmail");
+        String myEmail = "moonb@gmail.com";
+        User user = myPageService.updateForm(myEmail);
+        System.out.println("test : "+user.getUserName());
+        model.addAttribute("userInfo", user);
+
+        return "mypage/editUser";
+    }
+
+    @GetMapping("/mypage")
+    public ModelAndView SignUp() {
+        ModelAndView mav = new ModelAndView("/mypage/mypage");
+        return mav;
+    }
+
+
+
+
 }
---
+++ src/main/resources/static/script/mypage/checkPassword.js
@@ -0,0 +1,27 @@
+$('#checkPassword').click(function(e) {
+        e.preventDefault();
+        const checkPassword = $('#password').val();
+        if(!checkPassword || checkPassword.trim() === ""){
+            alert("비밀번호를 입력하세요.");
+        } else{
+            $.ajax({
+                type: 'POST',
+                url: '/checkPassword',
+                data: {'checkPassword': checkPassword},
+                datatype: "text"
+            }).done(function(result){
+                console.log(result);
+                if(result){
+                    console.log("비밀번호 일치");
+                    window.location.href="editUser";
+                } else if(!result){
+                    console.log("비밀번호 틀림");
+                    // 비밀번호가 일치하지 않으면
+                    alert("비밀번호가 맞지 않습니다.");
+                    window.location.reload();
+                }
+            }).fail(function(error){
+                alert(JSON.stringify(error));
+            })
+        }
+    });

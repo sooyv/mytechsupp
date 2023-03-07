00211277adc802a1a5016c55b91665254a726106
thumbthing <ilscc0902@gmail.com>
2023-03-07 12:39:06 +0900

    delete: NoticeImageFileDTO.java (사용 안하는 클래스 삭제)

--- src/main/java/techsuppDev/techsupp/repository/AdminProductRepository.java
+++ src/main/java/techsuppDev/techsupp/repository/AdminProductRepository.java
@@ -1,7 +1,9 @@
 package techsuppDev.techsupp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import techsuppDev.techsupp.domain.Product;
 
+@Repository
 public interface AdminProductRepository extends JpaRepository<Product,Long> {
 }

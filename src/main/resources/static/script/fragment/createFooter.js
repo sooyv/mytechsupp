8746cded32f5dfa84bcff976f4ccfb7db22a8185
thumbthing <ilscc0902@gmail.com>
2023-03-09 12:43:56 +0900

    feat: footer.js (footer 생성 스크립트) fix: feedbackmain.html , feedbackselect.html, feedbackwrite.html, productInvestComplete.html, productMain.html, productInvesting.html, productSelect.html (footer 생성 js 파일 추가)

---
+++ src/main/resources/static/script/fragment/createFooter.js
@@ -0,0 +1,6 @@
+const footer = document.querySelector('.ContainerFooter');
+
+footer.innerHTML = `
+<p class="float-end"><a href="#">Back to top</a></p>
+<p>© 2023 TECHSUPP · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+`;

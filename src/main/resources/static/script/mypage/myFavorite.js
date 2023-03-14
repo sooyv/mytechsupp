25de82636934f2ddc6910f5d986388eed4da59b3
moonbeom <tjansqja789@naver.com>
2023-03-14 17:45:17 +0900

    3/14 기능 end 서문범

--- src/main/resources/static/script/mypage/myFavorite.js
+++ src/main/resources/static/script/mypage/myFavorite.js
@@ -33,12 +33,14 @@ function nextBlock() {
   currentPage = Math.min(currentPage, totalPages);
   loadProducts();
   generatePageBlock();
+  history.pushState({}, '', `?page=${currentPage}`);
 }
 
 function prevBlock() {
   currentPage = Math.max(currentPage - 10, 1);
   loadProducts();
   generatePageBlock();
+  history.pushState({}, '', `?page=${currentPage}`);
 }
 
 function generatePageBlock() {
@@ -92,12 +94,19 @@ const urlParams = new URLSearchParams(window.location.search);
 const pageParam = urlParams.get('page');
 if (pageParam !== null) {
   currentPage = parseInt(pageParam);
+} else if (window.location.pathname === "/myfavorite") { // myfavorite 페이지에 처음 진입한 경우 currentPage를 1로 설정
+  currentPage = 1;
+  history.replaceState({}, '', '/user/myfavorite'); // 쿼리스트링을 추가하지 않고 경로만 변경
 }
+
 window.addEventListener("load", loadProducts);
 
 loadProducts();
 generatePageBlock();
-history.pushState({}, '', `?page=${currentPage}`); // 페이지 로드 시 쿼리스트링 추가
+if (currentPage !== 1) { // currentPage가 1일 때는 쿼리스트링을 추가하지 않음
+  history.pushState({}, '', `?page=${currentPage}`);
+}
+
 
 function goMyPage() {
     location.href = "mypage";

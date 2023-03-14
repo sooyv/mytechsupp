c46729da451b832086a8ea137054d589b0227017
thumbthing <ilscc0902@gmail.com>
2023-03-14 19:58:39 +0900

    fix : invest page 스타일 변경

--- src/main/resources/static/script/invest/createInvestForm.js
+++ src/main/resources/static/script/invest/createInvestForm.js
@@ -3,13 +3,15 @@ const investFrom = document.querySelector('.ContainerUserInput');
 function InvestForm(api) {
   investFrom.innerHTML = `
   <form class="InvestForm" action="">
-    아이디
+    <h5>아이디</h5>
     <input type="email" class="User_email" value="${api.userEmail}" name="user_email">
-    휴대폰 번호
+    
+    <h5>휴대폰 번호</h5>
     <input type="tel" class = "User_phone" value="${api.userPhone}" name="user_phone">
+    
     <div class="KakaomapContainer">
       <div class="Address">
-        주소
+        <h5>주소</h5>
         <input type="text" id="sample4_postcode" placeholder="우편번호" name="zip_code">
         <input type="button" onclick="sample4_execDaumPostcode()" value="우편번호 찾기">        
       </div>
@@ -18,10 +20,13 @@ function InvestForm(api) {
         <input type="text" id="sample4_detailAddress" placeholder="상세주소" name="detail_addr">
       </div>
       <div class = "PaymentContainer">
-        <input type="checkbox" name="payment" onclick="checkBox(this)" value = "card" placeholder="카드">
-        <input type="checkbox" name="payment"  onclick="checkBox(this)" value = "cash" placeholder="현금">
-      </div>
-     </form>
+        <h6>카드 현금</h6>
+        <div class = "PaymentRadio">
+          <input type="checkbox" name="payment" onclick="checkBox(this)" value = "card" placeholder="카드">
+          <input type="checkbox" name="payment"  onclick="checkBox(this)" value = "cash" placeholder="현금">
+        </div>
+    </div>
+    </form>
   `
 }
 
